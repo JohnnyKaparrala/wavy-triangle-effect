e69d9b02c3539e2bcf3edf1ccaecdbc5618de8c3
Johnny Kaparrala <30930747+JohnnyKaparrala@users.noreply.github.com>
2018-12-12 23:50:20 -0200

    a

--- js/efeito.js
+++ js/efeito.js
@@ -29,39 +29,94 @@ $(document).ready(function () {
 	c.width = $("#sup").parent().width() * c.width / 100;
 	c.height = $("#sup").parent().height() * c.height / 100;
 	let ctx = c.getContext("2d");
-
-	let tamanho = 100;
-
- 	let altura = c.scrollHeight;
+	let altura = c.scrollHeight;
  	let largura = c.scrollWidth;
 
-  let pontos = new Array();
-  let bonus = 10;
-	for (let i = -bonus; i < largura / tamanho + bonus; i ++) {
-		pontos[i+bonus] = new Array();
-		for (let j = -bonus; j < altura / tamanho + bonus; j ++) {
-			pontos[i+bonus][j+bonus] = new Ponto (tamanho * i, tamanho * j);
-		}
-	}
+ 	let pontos;
+ 	let pontosReferencia;
+ 	let triangulos;
 
-	$("#qtd").text("pontos: " + pontos.length * pontos[0].length);
+ 	init(150);
 
-	//comeca a formar os triangulos
-	let triangulos = new Array();
-	let triCont = 0;
-	for (let i = 0; i < pontos.length; i++) {
+	function randomize (pontos, pontosReferencia, raio) {
+		for (let i = 0; i < pontos.length; i++) {
 			for (let j = 0; j < pontos[i].length; j++) {
-				if (i < pontos.length - 1 && j < pontos[i].length - 1) {
-					console.log((i+1) + ", " + (j+1));
-					triangulos[triCont] = new Triangulo(pontos[i][j], pontos[i][j+1], pontos[i+1][j+1]);
-					triCont++;
-					triangulos[triCont] = new Triangulo(pontos[i][j], pontos[i+1][j], pontos[i+1][j+1]);
-					triCont++;
-				}
+				let angulo = Math.random() * Math.PI * 2;
+				let pt_radius_sq = Math.random() * raio * raio;
+		    let pt_x = Math.sqrt(pt_radius_sq) * Math.cos(angulo);
+		    let pt_y = Math.sqrt(pt_radius_sq) * Math.sin(angulo);
+				pontos[i][j].x = pt_x + pontosReferencia[i][j].x;
+				pontos[i][j].y = pt_y + pontosReferencia[i][j].y;
+			}
 		}
 	}
 
-	desenharTriangulos(triangulos);
+	function gerarNovoPonto (pontoRef, raio) {
+		let angulo = Math.random() * Math.PI * 2;
+		let pt_radius_sq = Math.random() * raio * raio;
+    let pt_x = Math.sqrt(pt_radius_sq) * Math.cos(angulo);
+    let pt_y = Math.sqrt(pt_radius_sq) * Math.sin(angulo);
+		return new Ponto(pontoRef.x + pt_x, pontoRef.y + pt_y);
+	}
+
+	function init (raio) {
+		let tamanho = raio;
+
+
+	  pontos = new Array();
+	  let bonus = 10;
+		for (let i = -bonus; i < largura / tamanho + bonus; i ++) {
+			pontos[i+bonus] = new Array();
+			for (let j = -bonus; j < altura / tamanho + bonus; j ++) {
+				pontos[i+bonus][j+bonus] = new Ponto (tamanho * i, tamanho * j);
+			}
+		}
+
+		pontosReferencia = new Array();
+		for (let i = -bonus; i < largura / tamanho + bonus; i ++) {
+			pontosReferencia[i+bonus] = new Array();
+			for (let j = -bonus; j < altura / tamanho + bonus; j ++) {
+				pontosReferencia[i+bonus][j+bonus] = new Ponto (tamanho * i, tamanho * j);
+			}
+		}
+
+		//comeca a formar os triangulos
+		triangulos = new Array();
+		let triCont = 0;
+		for (let i = 0; i < pontos.length; i++) {
+				for (let j = 0; j < pontos[i].length; j++) {
+					if (i < pontos.length - 1 && j < pontos[i].length - 1) {
+						console.log((i+1) + ", " + (j+1));
+						triangulos[triCont] = new Triangulo(pontos[i][j], pontos[i][j+1], pontos[i+1][j+1]);
+						triCont++;
+						triangulos[triCont] = new Triangulo(pontos[i][j], pontos[i+1][j], pontos[i+1][j+1]);
+						triCont++;
+					}
+			}
+		}
+		randomize(pontos, pontosReferencia, 60);
+		passo();
+	}
+
+	function passo () {
+		//setar um destino, se ja tiver um, andar ate ele, para cada ponto
+		desenharTriangulos(triangulos);
+    window.requestAnimationFrame(passo);
+	}
+
+	function calcSpeed(prev, next) {
+    
+    let x = Math.abs(prev.x - next.x);
+    let y = Math.abs(prev.y - next.y);
+    
+    let greatest = x > y ? x : y;
+    
+    let speedModifier = 0.1;
+
+    let speed = Math.ceil(greatest/speedModifier);
+
+    return speed;
+	}
 
 	function desenharTriangulos (triangulos) {
 		let pontosDeCor = new Array();
@@ -112,20 +167,4 @@ $(document).ready(function () {
 			ctx.fill();
 		}
 	}
-
-	function random (pontos, raio) {
-		for (let i = 0; i < pontos.length; i++) {
-			for (let j = 0; j < pontos[i].length; j++) {
-				let angulo = Math.random() * Math.PI * 2;
-				pontos[i][j].x += Math.cos(angulo) * raio;
-				pontos[i][j].y += Math.sin(angulo) * raio;
-			}
-		}
-	}
-
-	$("#botao").click( function (){
-		random(pontos, 20);
-		ctx.clearRect(0, 0, c.width, c.height);
-		desenharTriangulos(triangulos);
-	})
 })
